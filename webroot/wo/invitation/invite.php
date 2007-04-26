0793324f4e9accaf54d98ee30f612a9df86d5908
Zhuohuan LI <zixia@zixia.net>
2007-04-26 09:01:17 +0000

    skip unfinished invite page

--- webroot/wo/invitation/invite.php
+++ webroot/wo/invitation/invite.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <?php
+header("Location: /");
+exit();
+
 require_once('../../jiwai.inc.php');
 
 $debug = JWDebug::instance();
