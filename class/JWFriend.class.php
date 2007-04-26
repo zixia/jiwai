cc304d28e5d0790de96b4ed857ad5ce83f8ca9bd
Zhuohuan LI <zixia@zixia.net>
2007-04-26 09:00:00 +0000

    new we have friend function supported!

---
+++ class/JWFriend.class.php
@@ -0,0 +1,204 @@
+<?php
+/**
+ * @package		JiWai.de
+ * @copyright	AKA Inc.
+ * @author	  	zixia@zixia.net
+ * @version		$Id$
+ */
+
+/**
+ * JiWai.de Friend Class
+ */
+class JWFriend {
+	/**
+	 * Instance of this singleton
+	 *
+	 * @var JWFriend
+	 */
+	static private $msInstance;
+
+	/**
+	 * Instance of this singleton class
+	 *
+	 * @return JWFriend
+	 */
+	static public function &Instance()
+	{
+		if (!isset(self::$msInstance)) {
+			$class = __CLASS__;
+			self::$msInstance = new $class;
+		}
+		return self::$msInstance;
+	}
+
+
+	/**
+	 * Constructing method, save initial state
+	 *
+	 */
+	function __construct()
+	{
+	}
+
+
+	/**
+	 * Is idFriend is idUser's friend?
+	 *
+	 */
+	static function IsFriend($idUser, $idFriend=null)
+	{
+		if ( !is_int($idUser) )
+			$idUser 	= intval($idUser);
+
+		if ( !is_int($idFriend) )
+			$idFriend 	= intval($idFriend);
+
+		if ( 0==$idUser || 0==$idFriend )
+			throw new JWException("need int id");
+
+		return JWDB::ExistTableRow('Friend', array('idUser'=>$idUser,'idFriend'=>$idFriend));
+	}
+
+
+	/**
+	 * Is idFollower is idUser's follower?
+	 *
+	 */
+	static function IsFollower($idUser, $idFollower=null)
+	{
+		// TODO
+		return true;
+	}
+
+
+	/**
+	 * Get friend list
+	 *	@return array	array of friend id list
+	 */
+	static function GetFriend($idUser, $numMax=40)
+	{
+		$idUser = intval($idUser);
+		$numMax = intval($numMax);
+
+		if ( 0==$idUser || 0==$numMax )
+			throw new JWException('not int');
+
+		$sql = <<<_SQL_
+SELECT	idFriend
+FROM	Friend
+WHERE	idUser=$idUser
+LIMIT	$numMax
+_SQL_;
+
+		$arr_result = JWDB::GetQueryResult($sql, true);
+
+		if ( empty($arr_result) )
+		{
+			return null;
+		}
+
+		$arr_friend_id = array();
+		foreach ( $arr_result as $row )
+			array_push($arr_friend_id, $row['idFriend']);
+
+		return $arr_friend_id;
+	}
+
+
+	/*
+	 *	idUser 删除好友 idFriend
+	 * @param	int	idFriend
+	 * @param	int	idUser
+	 * @return 
+			true: 成功 
+			false: 失败
+	 */
+	static public function Destroy($idFriend, $idUser)
+	{
+		if ( !is_int($idUser) )
+			$idUser 	= intval($idUser);
+
+		if ( !is_int($idFriend) )
+			$idFriend 	= intval($idFriend);
+
+
+		if ( !is_int($idFriend) || !is_int($idUser ) )
+			throw new JWException("id not int");
+
+		$sql = <<<_SQL_
+DELETE FROM	Friend
+WHERE 		idUser=$idUser
+			AND idFriend=$idFriend
+_SQL_;
+
+		try
+		{
+			$result = JWDB::Execute($sql) ;
+		}
+		catch(Exception $e)
+		{
+			JWDebug::trace( $e );
+			return false;
+		}
+
+		return true;
+	}
+
+
+	/*
+	 *	idUser 添加 idFriend 为好友
+	 * @param	int	idFriend
+	 * @param	int	idUser
+	 * @return 
+			true: 成功 
+			false: 失败
+	 */
+	static public function Create($idFriend, $idUser)
+	{
+		if ( !is_int($idFriend) || !is_int($idUser ) )
+			throw new JWException("id not int");
+
+		$sql = <<<_SQL_
+INSERT INTO	Friend
+SET 		idUser=$idUser
+			, idFriend=$idFriend
+_SQL_;
+
+		try
+		{
+			$result = JWDB::Execute($sql) ;
+		}
+		catch(Exception $e)
+		{
+			JWDebug::trace( $e );
+			return false;
+		}
+
+		return true;
+	}
+
+	/*
+	 *	@param	int		$idUser
+	 *	@return	int		$friendNum for $idUser
+	 */
+	static public function GetFriendNum($idUser)
+	{
+		$idUser = intval($idUser);
+
+		if ( !is_int($idUser) )
+			throw new JWException('must be int');
+
+		$sql = <<<_SQL_
+SELECT	COUNT(*) as num
+FROM	Friend
+WHERE	idUser=$idUser
+_SQL_;
+		$row = JWDB::GetQueryResult($sql);
+
+		return $row['num'];
+	}
+
+
+
+}
+?>
