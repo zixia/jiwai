e679d1e35c3d65526c53332141aac63599e59db1
Shuwen DAI <shwdai@gmail.com>
2007-04-29 05:05:34 +0000

    translate and template

--- webroot/wo/account/password_reset.php
+++ webroot/wo/account/password_reset.php
@@ -1,53 +1,35 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta http-equiv="Content-Language" content="en-us" />
-	<title>Twitter</title>
+<?php
+require_once('../../../jiwai.inc.php');
+?>
+<html>
 
-  <link href="http://assets0.twitter.com/stylesheets/screen.css?1176244562" media="screen, projection" rel="Stylesheet" type="text/css" />
-	    <link href="http://assets1.twitter.com/stylesheets/ie.css?1176244562" media="screen, projection" rel="Stylesheet" type="text/css" />
-  
-	<link rel="shortcut icon" href="http://assets3.twitter.com/images/favicon.ico?1176244562" type="image/x-icon" />
-  
-	
-</head>
+<?php JWTemplate::html_head() ?>
+
 <body class="account" id="resend_password">
-	<style type="text/css">
-	
-    body {background: #9ae4e8 url(http://assets2.twitter.com/images/bg.gif?1176244562) fixed no-repeat top left; text-align: center; font: 0.75em/1.5 Helvetica, Arial, sans-serif; color: #333;}
-	  </style>
-	
-	<ul id="accessibility">
-		<li>
-			<a href="#navigation" accesskey="2">Skip to navigation</a>
-		</li>
-		<li>
-			<a href="#side">Skip to sidebar</a>
-		</li>
-	</ul>
-	
-	<div id="container" class="subpage">
-		<h1 id="header">
-	<a href="/" title="Twitter: home" accesskey="1">
-				<img alt="Twitter.com" height="49" src="http://assets3.twitter.com/images/twitter.gif?1176244562" width="210" />
-			</a>
-</h1>
-		
-				
-		<div id="content"><div class="wrapper">	
-			<h2>Forgot?</h2>
 
-<p>Put in your email address below and we&rsquo;ll send it to you.</p>
+<?php JWTemplate::accessibility() ?>
+
+<?php JWTemplate::header() ?>
+<div class="separator"></div>
+
+<div id="container" class="subpage">
+	<div id="content">
+		<div id="wrapper">
+
+
+			<h2>忘记了？</h2>
+
+<p>请输入您的 Email 地址，我们将把密码重设的链接发给您。</p>
 
 <form action="/account/resend_password" method="post" name="f">
 <fieldset>
 <table>
 	<tr>
-		<th><label for="email">Email:</label></th>
+		<th><label for="email">Email 地址：</label></th>
 		<td><input id="email" name="email" type="text" /></td>
 	</tr>
-	<tr><th></th><td><input name="commit" type="submit" value="Send my password" /></td></tr>
+	<tr><th></th><td><input name="commit" type="submit" value="将重设密码地址发给我！" /></td></tr>
 </table>
 </fieldset>
 </form>
@@ -60,24 +42,14 @@ $('email').focus();
 
 			
 		
-		<hr />
-		<div id="footer">
-	<h3>Footer</h3>
-	<ul>
-		<li class="first">&copy; 2007 Obvious</li>
-		<li><a href="/help/aboutus">About Us</a></li>
-		<li><a href="/help/contact">Contact</a></li>
-		<li><a href="http://twitter.com/blog">Blog</a></li>
-		<li><a href="/help/api">API</a></li>
-		<li><a href="/help">Help</a></li>
-		<li><a href="/tos">Terms of Service</a></li>
-	</ul>
-</div>
-			</div>
-	<script src="http://www.google-analytics.com/urchin.js" type="text/javascript"></script>
-	<script type="text/javascript">
-	  _uacct = "UA-30775-6";
-	  urchinTracker();
-	</script>
-<!-- served to you through a copper wire by kolea.twitter.com at 11 Apr 15:45 in 15 ms (d 0 / r 14). thank you, come again. -->
-</body></html>
+
+		</div><!-- wrapper -->
+	</div><!-- content -->
+
+</div><!-- #container -->
+<hr class="separator" />
+
+<?php JWTemplate::footer() ?>
+
+</body>
+</html>
