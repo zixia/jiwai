496dd606c1f3ea4febf4c129490becc14a1139ae
Zhuohuan LI <zixia@zixia.net>
2007-05-03 10:50:25 +0000

    google analytics 澧炲姞浜嗙己鐪佺殑缃戠珯uOsr锛岋鎶婅嚜瀹氫箟鐨勫悜鍚庢尓

--- class/JWTemplate.class.php
+++ class/JWTemplate.class.php
@@ -164,15 +164,16 @@ class JWTemplate {
 </script>
 <script type="text/javascript">
 _uacct = "UA-287835-11";
-_uOsr[20]="baidu"; _uOkw[20]="wd";
-_uOsr[21]="3721"; _uOkw[21]="name";
-_uOsr[22]="baidu"; _uOkw[22]="word";
-_uOsr[23]="qq.com"; _uOkw[23]="w";
 _uOsr[24]="iask"; _uOkw[24]="k";
 _uOsr[25]="sogou"; _uOkw[25]="query";
 _uOsr[26]="qihoo"; _uOkw[26]="kw";
 _uOsr[27]="daqi"; _uOkw[27]="content";
 _uOsr[28]="soso.com"; _uOkw[28]="w";
+_uOsr[29]="baidu"; _uOkw[29]="wd";
+_uOsr[30]="3721"; _uOkw[30]="name";
+_uOsr[31]="baidu"; _uOkw[31]="word";
+_uOsr[32]="qq.com"; _uOkw[32]="w";
+
 urchinTracker();
 </script>
 <?php
