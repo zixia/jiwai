a4d2c54d7ba84bd683c7c6911aa46088855bacc8
Zhuohuan LI <zixia@zixia.net>
2007-05-08 15:15:46 +0000

    Yellow Fade Technique

--- domain/asset/js/jiwai.js
+++ domain/asset/js/jiwai.js
@@ -1,23 +1,74 @@
-function loadAds(module) {
-    /* Google */
-   
-    setTimeout("run_google();", 500);
- 
+/*
+ *	JiWai.de Lib
+ *	Author: zixia@zixia.net
+ *	AKA Inc.
+ *	2007-05
+ */
+
+var JiWai = 
+{ 
+	mVersion 	: 1,
+
+	GetBgColor : function () 
+	{
+   		var id_element = $(arguments[0]);
+
+		var color;
+		var n=0;
+		while ( 100>n++ ) 
+		{
+			try {
+				color = id_element.getStyle('background-color');
+			} catch ( e ) {
+				break;
+			}
+
+			id_element = $(id_element.parentNode);
+
+			if ( 'transparent'!=color )
+				break;
+		}
+
+		if ( 'transition'==color )
+			color = '#fff';
+
+		return color;
+	},
+
+	Yft		: function (idElement) 
+	{
+		this.mIdElement = idElement;
+
+		background_color = $(idElement).getStyle('background-color');
+
+		orig_color 		= this.GetBgColor(idElement);
+
+		yellow_color	= new Color('#ff0');
+		yellow_color	= yellow_color.mix(orig_color);
+
+		//$(idElement).effect('background-color').start('#f00',orig_color);
+
+		$(idElement).effect(
+			'background-color'
+			,{
+			 	duration: 1500
+				,transition: Fx.Transitions.Quad.easeOut
+			}
+		).start(
+			orig_color
+			,yellow_color
+		).chain( function () {
+			$(idElement).effect(
+				 'background-color'
+				,{
+					 duration: 1500
+					,transition: Fx.Transitions.Bounce.easeOut
+				}
+			).start(yellow_color,orig_color).chain( function () {
+				$(idElement).setStyle('background-color', background_color);
+			})
+		});
+	}
+
 }
 
-function run_google() {
-    if (!window.urchinTracker) {
-        setTimeout(run_google, 500);
-        return;
-    }
-    _uacct = "UA-287835-11";
-    urchinTracker();
-}
-/*
-<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
-</script>
-<script type="text/javascript">
-_uacct = "UA-287835-11";
-urchinTracker();
-</script>
-*/
