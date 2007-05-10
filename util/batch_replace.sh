e61e81e1eb03af71865f3c8cfd052272aec31648
Zhuohuan LI <zixia@zixia.net>
2007-05-10 18:46:10 +0000

    util

---
+++ util/batch_replace.sh
@@ -0,0 +1,12 @@
+find . -type f -name '*.php' -print | grep -v svn | 
+  while read filename
+  do
+    (
+    echo "processing $filename ..."
+    perl -e 'while(<>){s/JWUser::GetCurrentUserId/JWLogin::GetCurrentUserId/i;print;}' < $filename > $filename.xxxxx
+    echo "baking $filename ..."
+    mv $filename $filename.replace.bak
+    echo "renaming $filename ..."
+    mv $filename.xxxxx $filename # replace output files with original
+    )
+done
