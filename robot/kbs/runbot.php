04c184114195d70a1200819e6b79f1901753f8af
Song DU <freewizard@gmail.com>
2007-05-25 05:06:00 +0000

    seperate bot define and routines

--- robot/kbs/runbot.php
+++ robot/kbs/runbot.php
@@ -1,10 +1,4 @@
 <?php
-define('BBS_URL',      'http://www4.newsmth.net');
-define('BBS_ID',       'JiWai');
-define('BBS_PASSWD',   '123789');
-define('BBS_JWNAME',   'newsmth');
-define('BBS_JWDOMAIN', 'newsmth.net');
-
 require_once '../../jiwai.inc.php';
 
 /*
---
+++ robot/kbs/smth.php
@@ -0,0 +1,9 @@
+<?php
+define('BBS_URL',      'http://www4.newsmth.net');
+define('BBS_ID',       'JiWai');
+define('BBS_PASSWD',   '123789');
+define('BBS_JWNAME',   'newsmth');
+define('BBS_JWDOMAIN', 'newsmth.net');
+
+require_once 'runbot.php';
+?>
