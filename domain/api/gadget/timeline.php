f2d14380f8cb1816808965be630d66f4bccf3f77
Zhuohuan LI <zixia@zixia.net>
2007-05-29 02:16:43 +0000

    fix time show in gadget

--- domain/api/gadget/timeline.php
+++ domain/api/gadget/timeline.php
@@ -198,6 +198,9 @@ jiwai_de_gadget 				= document.getElementById("$gadgetDivId")
 
 function relative_time(time_value) 
 {
+    var values = time_value.split(" ");
+    time_value = values[1] + " " + values[2] + ", " + values[5] + " " + values[3];
+
 	var parsed_date = Date.parse(time_value);
 
 	var relative_to = (arguments.length > 1) ? arguments[1] : new Date();
