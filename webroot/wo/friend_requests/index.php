286fb1913071e8e3adbad9bfb9b4b66d7922e912
Zhuohuan LI <zixia@zixia.net>
2007-05-30 07:59:43 +0000

    add new dir

---
+++ webroot/wo/friend_requests/index.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta http-equiv="Content-Language" content="en-us" />
+	<title>Twitter</title>
+
+  <link href="http://assets2.twitter.com/stylesheets/screen.css?1178587735" media="screen, projection" rel="Stylesheet" type="text/css" />
+	
+	<link rel="shortcut icon" href="http://assets1.twitter.com/images/favicon.ico?1178587735" type="image/x-icon" />
+  <script src="http://assets1.twitter.com/javascripts/prototype.js?1178587735" type="text/javascript"></script>
+
+<script src="http://assets3.twitter.com/javascripts/effects.js?1178587735" type="text/javascript"></script>
+<script src="http://assets3.twitter.com/javascripts/application.js?1178587735" type="text/javascript"></script>
+	
+</head>
+<body class="friend_requests" id="friend_requests">
+	<style type="text/css">
+			a {color: #0000ff;}
+		body {
+			color: #000000;
+			background-color: #9ae4e8;
+						background: #9ae4e8 url(http://assets1.twitter.com/images/bg.gif?1178587735) fixed no-repeat top left;
+			      text-align: center; 
+      font: 0.75em/1.5 Helvetica, Arial, sans-serif; 
+		}
+		#side {
+			background-color: #e0ff92;
+			border: 1px solid #87bc44;
+		}
+		#side .notify {border: 1px solid #87bc44;}
+		#side .actions {border: 1px solid #87bc44;}
+		h2.thumb, h2.thumb a {color: #000000;}
+    </style>
+	
+	<ul id="accessibility">
+    <li>
+      On a mobile phone? Check out <a href="http://m.twitter.com/">m.twitter.com</a>!
+    </li>
+
+		<li>
+			<a href="#navigation" accesskey="2">Skip to navigation</a>
+		</li>
+		<li>
+			<a href="#side">Skip to sidebar</a>
+		</li>
+	</ul>
+	
+	<div id="container" class="subpage">
+
+		<h1 id="header">
+	<a href="/" title="Twitter: home" accesskey="1">
+				<img alt="Twitter.com" height="49" src="http://assets0.twitter.com/images/twitter.png?1178587735" width="210" />
+			</a>
+</h1>
+		
+				
+		<div id="content"><div class="wrapper">	
+			<h2>New Friend Requests</h2>
+
+<table id="timeline" class="doing" cellspacing="0">
+	<tr class="odd">
+
+	<td class="thumb">
+		<a href="http://twitter.com/zixia"><img alt="Dsc01663_1024x768" src="http://assets3.twitter.com/system/user/profile_image/762460/normal/DSC01663_1024x768.jpg?1177944712" /></a>
+	</td>
+	<td>
+		<a href="http://twitter.com/zixia">Zhuohuan Li</a>
+		
+		<p class="friend-request">
+			&rarr;
+			<a href="/friend_requests/accept/687032">Accept</a> |
+			<a href="/friend_requests/deny/687032">Deny</a>
+
+		</p>
+	</td>
+</tr>
+</table>
+		</div></div><hr />
+
+			
+		
+		<div id="navigation">
+	<h3>Navigation</h3>
+	<ul>
+		<li class="first"><a href="http://twitter.com/home">Home</a></li>
+
+		<li><a href="http://twitter.com/zixia">Your profile</a></li>
+		<li><a href="/invitations/invite">Invite</a></li>
+		<li><a href="/public_timeline">Public timeline</a></li>
+		<li><a href="/account/badge">Badges</a></li>
+		<li><a href="/account/settings">Settings</a></li>
+		<li><a href="http://help.twitter.com" target="_blank">Help</a></li>
+
+    <li><a href="/logout?siv=5386775d96a2f09e52353cab2c07b6e1">Sign out</a></li>
+			</ul>
+</div>
+<hr />
+		<div id="footer">
+	<h3>Footer</h3>
+	<ul>
+		<li class="first">&copy; 2007 Twitter</li>
+
+		<li><a href="/help/aboutus">About Us</a></li>
+		<li><a href="/help/contact">Contact</a></li>
+		<li><a href="http://twitter.com/blog">Blog</a></li>
+		<li><a href="/help/api">API</a></li>
+		<li><a href="/help">Help</a></li>
+		<li><a href="/tos">Terms of Service</a></li>
+
+	</ul>
+</div>
+
+			</div>
+	
+  
+	<script src="http://www.google-analytics.com/urchin.js" type="text/javascript"></script>
+	<script type="text/javascript">
+	  _uacct = "UA-30775-6";
+	  urchinTracker();
+	</script>
+	
+<!-- served to you through a copper wire by donald at 08 May 07:13 in 14 ms (d 2 / r 11). thank you, come again. -->
+</body></html>
