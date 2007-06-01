bb679d754f65513f1b86ef97331f4599387cee4b
Zhuohuan LI <zixia@zixia.net>
2007-06-01 12:45:30 +0000

    modify error page

--- webroot/wo/error/404.php
+++ webroot/wo/error/404.php
@@ -21,5 +21,72 @@ if ( empty($url) )
 	exit(0);
 }
 
-echo "哎呀！我找不到 <a href='$url'>$url</a> 啦！:-(";
+JWLog::Log(LOG_CRIT, "404URL: $url");
+
+JWTemplate::html_doctype();
+
+
+JWTemplate::html_head();
+
 ?>
+
+
+
+  <body>
+
+<?php JWTemplate::header() ?>
+
+<div class="separator"></div>
+
+<div id="container">
+
+<style type="text/css">
+table td { margin: 10px; padding:10px; }
+table h2 { font-size:20px; margin:10px; padding:10px; margin-left: 0px;padding-left: 0px; }
+table h3 { font-size:28px; margin:10px; padding:10px; margin-left: 0px;padding-left: 0px; }
+table .right { text-align:top }
+
+table ul {
+margin: 20px;
+font-size: 1.5em;
+list-style-image:none;
+list-style-position:outside;
+list-style-type:circle;
+}
+
+table li {
+margin: 10px;
+}
+
+</style>
+	<table>
+	<tr>
+	<td class="left">
+    <a href="/"><img src="<?php echo JWTemplate::GetAssetUrl('/img/system/-_-b.jpg')?>"/></a>
+	</td>
+	<td class="right">
+
+    <h2>哎呀！<a href="/">叽歪de</a> 没能找到<a href="<?php echo $url?>">这个页面</a>。</h2>
+    <br />
+    <br />
+
+    <h3>您可以：</h3>
+    <ul>
+      <li><a href="/">返回首页</a></li>
+
+      <li><a href="<?php echo JWTemplate::GetConst('UrlHelp')?>">查看帮助</a></li>
+      <li><a href="<?php echo JWTemplate::GetConst('UrlHelpComments')?>">向我们提问</a></li>
+    </ul>
+	</td>
+	</tr>
+ 	</table>
+
+
+</div><!-- #container -->
+
+<hr class="separator" />
+
+<?php JWTemplate::footer() ?>
+
+</body>
+</html>
--- webroot/wo/error/500.php
+++ webroot/wo/error/500.php
@@ -1,7 +1,6 @@
 <?php
 require_once('../../../jiwai.inc.php');
 
-
 if ( array_key_exists('500URL',$_SESSION) )
 {
 	$url = $_SESSION['500URL'];
@@ -18,15 +17,83 @@ else if ( isset($_SERVER['HTTP_REFERER']) )
 
 if ( empty($url) )
 {
-	//header('Location: /');
-	//exit(0);
+	header('Location: /');
+	exit(0);
 }
 
-echo <<<_HTML_
+
+
+JWLog::Log(LOG_CRIT, "500URL: $url");
+
+JWTemplate::html_doctype();
+
+
+JWTemplate::html_head();
+
+?>
+
+
+
+  <body>
+
+<?php JWTemplate::header() ?>
+
+<div class="separator"></div>
+
+<div id="container">
+
+<style type="text/css">
+table td { margin: 10px; padding:10px; }
+table h2 { font-size:20px; margin:10px; padding:10px; margin-left: 0px;padding-left: 0px; line-height:120%}
+table h3 { font-size:28px; margin:10px; padding:10px; margin-left: 0px;padding-left: 0px; }
+table .right { text-align:top }
+
+table ul {
+margin: 20px;
+font-size: 1.5em;
+list-style-image:none;
+list-style-position:outside;
+list-style-type:circle;
+}
+
+table li {
+margin: 10px;
+}
+
+</style>
+	<table>
+	<tr>
+	<td class="left">
+    <a href="/"><img src="<?php echo JWTemplate::GetAssetUrl('/img/system/cry.jpg')?>"/></a>
+	</td>
+	<td class="right">
+
+    <h2>
 <a href="/dev/">叽歪de系统核心</a>惨叫一声：
 “
 哇咧！我要被累死了，
-你等会儿再来吧……
+您等会儿再来吧……
 ”
-_HTML_;
-?>
+   <br />
+    <br />
+
+    <h3>您可以：</h3>
+    <ul>
+      <li><a href="/">返回首页</a></li>
+
+      <li><a href="<?php echo JWTemplate::GetConst('UrlHelp')?>">查看帮助</a></li>
+      <li><a href="<?php echo JWTemplate::GetConst('UrlHelpComments')?>">向我们提问</a></li>
+    </ul>
+	</td>
+	</tr>
+ 	</table>
+
+
+</div><!-- #container -->
+
+<hr class="separator" />
+
+<?php JWTemplate::footer() ?>
+
+</body>
+</html>
