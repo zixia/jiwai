461df38c2c95b9a5807321e4b5e5d84228d9d906
Zhuohuan LI <zixia@zixia.net>
2007-06-08 09:52:18 +0000

    add msn err notice

--- class/JWTemplate.class.php
+++ class/JWTemplate.class.php
@@ -341,6 +341,10 @@ $('status-field-char-counter').innerHTML = getStatusTextCharLengthMax($('status'
 //]]>
 			</script>
 
+		<p class="notice">
+			叽歪de MSN 机器人目前正在偷懒，我们会很快将她找回来的。<br />
+			请您先暂时使用 Web / QQ / GTalk 进行更新。
+		</p>
 <?php
 	}
 
