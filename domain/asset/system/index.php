536517ad71a9c4e0c11139337b0a0c4f123bb69d
Zhuohuan LI <zixia@zixia.net>
2007-06-17 14:13:24 +0000

    add asset image php move mootools to new dir

--- domain/asset/system/index.php
+++ domain/asset/system/index.php
@@ -6,12 +6,13 @@ require_once("../../../jiwai.inc.php");
 require_once(JW_ROOT . "webroot/user/picture.inc.php");
 
 
-if ( preg_match('#^user/profile_image/(?P<id_or_name>\w+)/(?P<pic_size>\w+)/#',$pathParam,$matches) )
+if ( preg_match('#^user/profile_image/(?P<id_or_name>\w+)/(?P<pic_id>\w+)/(?P<pic_size>\w+)/#',$pathParam,$matches) )
 {
 	$id_or_name = $matches['id_or_name'];
+	$pic_id 	= $matches['pic_id'];
 	$pic_size 	= $matches['pic_size'];
 
-	user_picture($id_or_name, $pic_size);
+	JWPicture::Show($pic_id, $pic_size);
 	exit(0);
 }
 ?>
