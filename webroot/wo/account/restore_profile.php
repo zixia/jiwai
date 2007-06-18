e464f03daee88a37b89e0b2d8cad852392b4289e
Zhuohuan LI <zixia@zixia.net>
2007-06-18 12:17:50 +0000

    add restore profile

---
+++ webroot/wo/account/restore_profile.php
@@ -0,0 +1,17 @@
+<?php
+require_once('../../../jiwai.inc.php');
+JWLogin::MustLogined();
+
+$user_id		= JWLogin::GetCurrentUserId();
+
+$ui = new JWDesign($user_id);
+$ui->Destroy();
+
+if ( array_key_exists('HTTP_REFERER',$_SERVER) )
+    $redirect_url = $_SERVER['HTTP_REFERER'];
+else
+    $redirect_url = '/wo/account/profile_settings';
+
+header('Location: ' . $redirect_url);
+exit(0);
+?>
