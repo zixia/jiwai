5a4be629596cf1dacef03996f8be36e650a368ee
Shuwen DAI <shwdai@gmail.com>
2007-07-27 01:13:59 +0000

    monitor with smsalert,syslog

--- robot/monitor/runmonitorgtalk.sh
+++
@@ -1,7 +0,0 @@
-/usr/java/jdk/bin/java -cp .:../../javaLib/jiwaiMessage.jar:../../javaLib/smack.jar:../../javaLib/smackx.jar GTalkMonitorRobot
-RVALUE=$?
-if [ $RVALUE -eq 0 ]; then
-	echo "Success"
-else
-	echo "Failed";
-fi
--- robot/monitor/runmonitormsn.sh
+++
@@ -1,7 +0,0 @@
-/usr/java/jdk/bin/java -cp .:../../javaLib/cindy.jar:../../javaLib/commons-logging.jar:../../javaLib/jml-1.0b1.jar:../../javaLib/jiwaiMessage.jar MsnMonitorRobot
-RVALUE=$?
-if [ $RVALUE -eq 0 ]; then
-	echo "Success"
-else
-	echo "Failed";
-fi
