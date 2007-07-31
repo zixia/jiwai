207e27171ad961501b6c79794b1a9c06f9d26e4c
Shuwen DAI <shwdai@gmail.com>
2007-07-31 23:25:52 +0000

    Add msn online check, use: http://osi.hshh.org:8888/msn/[email]

--- robot/online/QueryThread.py
+++ robot/online/QueryThread.py
@@ -13,9 +13,14 @@ def qqOnlineTest( address ):
 		return True, time.time();
 	return False, time.time()
 
+def msnOnlineTest( address ):
+	f = urllib.urlopen('http://osi.hshh.org:8888/msn/%s' % (address,) )
+	if f.geturl().find( 'online' ) > 0 :
+		return True, time.time();
+	return False, time.time()
+
 def gtalkOnlineTest( address ):
 	try:
-		print address;
 		s = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
 		host = "localhost"
 		port = 55010
@@ -36,25 +41,30 @@ class QueryThread:
 		self.sock = sock
 		self.device = device
 		self.address = address
+	
+	def getStatus(self, set, func):
+		if False == set.has_key( self.address ):
+			return func( self.address )
+		else:
+			if time.time() - set[ self.address ][1] > Configure.delay:
+				return func( self.address )
+		return set[ self.address ];
 
 	def run(self):
 		if self.device == "qq" :
 			if self.address.isdigit() :
-				if False == Configure.qqOnline.has_key( self.address ):
-					Configure.qqOnline[ self.address ] = qqOnlineTest( self.address )
-				else:
-					if Configure.qqOnline[ self.address ][1] - time.time() > Configure.delay:
-						Configure.qqOnline[ self.address ] = qqOnlineTest( self.address )
-				status = Configure.qqOnline[ self.address ];
+				status = self.getStatus( Configure.qqOnline, qqOnlineTest )
+				Configure.qqOnline[ self.address ] = status
 			else:
 				status = ( False, 0 )
-		if self.device == "gtalk" :
-			if False == Configure.gtalkOnline.has_key( self.address ):
-				Configure.gtalkOnline[ self.address ] = gtalkOnlineTest( self.address )
-			else:
-				if Configure.gtalkOnline[ self.address ][1] - time.time() > Configure.delay:
-					Configure.gtalkOnline[ self.address ] = gtalkOnlineTest( self.address )
-			status = Configure.gtalkOnline[ self.address ];
+	 	elif self.device == "gtalk" :
+			status = self.getStatus( Configure.gtalkOnline, gtalkOnlineTest )
+			Configure.gtalkOnline[ self.address ] = status
+		elif self.device == "msn" :
+			status = self.getStatus( Configure.msnOnline, msnOnlineTest )
+			Configure.msnOnline[ self.address ] = status
+		else :
+		     	status = False, 0 
 
 		if True == status[0] :
 			self.sock.send( 'Y' )
