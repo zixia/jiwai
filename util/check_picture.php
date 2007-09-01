eea2317ef00fbbce5106b95eec4ee2da3f8ec97f
Shuwen DAI <shwdai@gmail.com>
2007-09-01 03:20:20 +0000

    check storage picture,walk all dirictory and do sth

---
+++ util/check_picture.php
@@ -0,0 +1,51 @@
+<?php
+require_once(dirname(__FILE__) . "/../jiwai.inc.php");
+
+$pic_root = "/750/reiserfs/jiwai/picture/0/";
+// 750/reiserfs/jiwai/picture/0/8/9
+
+list_file($pic_root);
+
+function list_file($dir)
+{
+//echo "-> $dir\n";
+	if ( ! is_dir($dir) )
+	{
+		echo $dir . "\n";
+		return;
+	}
+
+	$handle = opendir($dir);
+
+	while ( false !== ($file=readdir($handle)) )
+	{
+		if ( $file=='.' || $file=='..' )
+			continue;
+
+		if ( is_dir("$dir/$file") )
+		{
+			list_file("$dir/$file");
+		}
+		else
+		{
+			if ( preg_match("/^picture\.(...)$/i",$file,$matches) )
+			{
+				//echo "$dir/ /$file\n";
+				$src_file = "$dir/$file";
+				$dst_file = "$dir/thumb96.$matches[1]";
+
+				if ( ! file_exists($dst_file) )
+				{
+					echo "ConvertThumbnail96($src_file, $dst_file)\n";
+					JWPicture::ConvertThumbnail96($src_file, $dst_file);
+				}
+			}
+		}
+	}
+
+	closedir($handle);
+}
+
+
+
+?>
