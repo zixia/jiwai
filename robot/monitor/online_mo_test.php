f68421ea94ec344ab00173d99b185de0b8e2d221
Shuwen DAI <shwdai@gmail.com>
2007-09-05 15:58:30 +0000

    onlineScript check

---
+++ robot/monitor/online_mo_test.php
@@ -0,0 +1,9 @@
+#!/usr/bin/php
+<?php
+$count = trim(`ps aux | grep 'online_mo.php' | grep -v grep | wc -l`);
+if( $count == 4 ) {
+	exit(0);
+}
+echo "online_mo.php miss Java process call!";
+exit(1)
+?>
