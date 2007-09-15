04e08820f42af8a69fbfee220013dc973a6a69c1
Hongwei WANG <glinus@gmail.com>
2007-09-15 13:17:09 +0000

    add /wo/replies

---
+++ domain/m/template/wo/replies.tpl
@@ -0,0 +1,23 @@
+<!--{include header}-->
+<!--{include wo/update}-->
+
+<h2><a href="/wo/">最新叽歪</a>｜叽友回复</h2>
+<ul>
+<!--{foreach $statuses as $status}-->
+<li>
+    <a href="${buildUrl('/'.$users[$status['idUser']]['nameScreen'].'/')}">
+        ${htmlSpecialChars($users[$status['idUser']]['nameFull'])}
+    </a>: 
+    {$status['status']}
+    <span class="stamp">
+    ${JWStatus::GetTimeDesc($status['timeCreate'])}
+    通过
+    ${JWDevice::GetNameFromType($status['device'], $status['idPartner'])}
+    </span>
+</li>
+<!--{/foreach}-->
+</ul>
+{$pageString}
+
+<!--{include shortcut}-->
+<!--{include footer}-->
---
+++ domain/m/wo/replies.php
@@ -0,0 +1,43 @@
+<?php
+require_once( '../config.inc.php' );
+
+JWLogin::MustLogined();
+
+$loginedUserInfo = JWUser::GetCurrentUserInfo();
+$loginedIdUser 	= $loginedUserInfo['id'];
+$page = isset($_REQUEST['page']) ? intval($_REQUEST['page']) : 1;
+$page = ($page < 1 ) ? 1 : $page;
+
+
+$statusNum= JWStatus::GetStatusNumFromReplies($loginedIdUser);
+$pagination		= new JWPagination($statusNum, $page, 10);
+$statusData 	= JWStatus::GetStatusIdsFromReplies($loginedIdUser, $pagination->GetNumPerPage(), $pagination->GetStartPos() );
+
+$statusRows	= JWDB_Cache_Status::GetDbRowsByIds($statusData['status_ids']);
+$userRows		= JWUser::GetUserDbRowsByIds	($statusData['user_ids']);
+
+krsort( $statusRows );
+
+$statuses = array();
+foreach( $statusRows as $k=>$s){
+    $fs = JWStatus::FormatStatus( $s, false );
+    $s['status'] = $fs['status'];
+   // $s['status']  = preg_replace('/^@\s*([\w\._\-]+)/e',"buildReplyUrl('$1')", htmlSpecialChars($s['status']) );
+    $statuses[ $k ] = $s;
+}
+
+$friendsNum = JWFriend::GetFriendNum( $loginedUserInfo['id'] );
+$followersNum = JWFollower::GetFollowerNum( $loginedUserInfo['id'] );
+
+$shortcut = array( 'public_timeline', 'logout', 'my', 'message' , 'friends', 'index', 'replies');
+$pageString = paginate( $pagination, '/wo/' );
+JWRender::Display( 'wo/replies', array(
+    'loginedUserInfo' => $loginedUserInfo,
+    'users' => $userRows,
+    'statuses' => $statuses,
+    'friendsNum' => $friendsNum,
+    'followersNum' => $followersNum,
+    'shortcut' => $shortcut,
+    'pageString' => $pageString,
+));
+?>
