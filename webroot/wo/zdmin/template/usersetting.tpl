637420cb7778ac54d71e7e94bb12692be05a650b
Xiangping KONG <kxphp@126.com>
2007-11-01 10:03:09 +0000

    fixed #140

---
+++ webroot/wo/zdmin/template/usersetting.tpl
@@ -0,0 +1,21 @@
+<!--{include header}-->
+<h2>用户密码修改：</h2>
+<!--{include tips}-->
+<form action="usersetting" method="POST">
+<p align = "center">要修改的帐号名:
+<input type = "text" name="un" id="un" />
+</p>
+
+<p align = "center">要修改的ID号:
+<input type="text" name="id" id="idStatus"/>
+</p>
+<p align="center">
+新密码为:
+<input type="password" name="password" id="password"/>
+</p>
+<p align="center">
+<input type="submit" value="修改" />
+</p>
+</form>
+<!--{include footer}-->
+
--- webroot/wo/zdmin/usersetting.php
+++ webroot/wo/zdmin/usersetting.php
@@ -4,7 +4,41 @@ if(!defined('TPL_TEMPLATE_DIR')) define('TPL_TEMPLATE_DIR',dirname(__FILE__).'/t
 require_once('../../../jiwai.inc.php');
 require_once('./function.php');
 
+$id = null;
+$password = null;
+$oneResult = array();
+$id = @trim($_POST['id']);
+$un = @trim($_POST['un']);
+$password = @trim($_POST['password']);
+//extract($_POST,EXTR_IF_EXISTS);
+if( $_POST ){
+    if( $id && $un && $password){
+        $id = JWDB::CheckInt($id);
+        $oneResult = JWUser::GetUserInfo($un);
+        if($oneResult){
+            $outId = $oneResult['id'];    
+        }
+        if($id == $outId){           
+        
+        JWUser::ChangePassword($id,$password);
+        setTips('修改'.$un.'的密码成功！');
+        }
+        else{
+            setTips('用户ID与用户名不匹配，请重新输入!');    
+        }
+    }
+    else{
+        setTips('请输入完整!');
+            
+    }
+
+
+ header("Location:usersetting");
+ exit;
+}
 $render = new JWHtmlRender();
-$render->display("index", array(
+$render->display("usersetting", array(
+     'menu_nav' => 'usersetting', 
 			));
 ?>
+
