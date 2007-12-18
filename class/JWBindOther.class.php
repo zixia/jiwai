ae1eba0f73eee0a0819dc95d1b898f7cf3490c7b
Shuwen DAI <shwdai@gmail.com>
2007-12-18 07:34:57 +0000

    see #273 let use choose whether sync reply or confenrence status to twitter | fanfou

--- class/JWBindOther.class.php
+++ class/JWBindOther.class.php
@@ -17,69 +17,84 @@ class JWBindOther {
 	const POST_TWITTER = 'http://twitter.com/statuses/update.json';
 	const POST_FANFOU = 'http://api.fanfou.com/statuses/update.xml';
 
-	static public function Create( $idUser, $loginName='name', $loginPass='123456', $service='twitter' ) {
+	static public function Create( $user_id, $login_name='name', $login_pass='123456', $service='twitter', $options=array() ) 
+	{
+		if ( false==isset($options['sync_reply']) )
+			$options['sync_reply'] = 'Y';
 
-		$idUser = JWDB::CheckInt( $idUser );
+		if ( false==isset($options['sync_conference']) )
+			$options['sync_conference'] = 'Y';
+
+		$user_id = JWDB::CheckInt( $user_id );
 		$service = strtolower( $service );
 
-		switch( $service ) {
+		switch( $service ) 
+		{
 			case 'twitter':
-				$flag = self::CheckTwitter( $loginName, $loginPass );
+				$flag = self::CheckTwitter( $login_name, $login_pass );
 			break;
 			case 'fanfou':
-				$flag = self::CheckFanfou( $loginName, $loginPass );
+				$flag = self::CheckFanfou( $login_name, $login_pass );
 			break;
 			default:
 				$flag = false;
 		}
 
-		if( $flag ) {
-			$idExist = JWDB::ExistTableRow('BindOther', array( 
-				'idUser' => $idUser, 
+		if( $flag ) 
+		{
+			$exist_id = JWDB::ExistTableRow('BindOther', array( 
+				'idUser' => $user_id, 
 				'service' => $service,
 			));
 
-			$uArray = array(
-				'idUser' => $idUser, 
+			$up_array = array(
+				'idUser' => $user_id, 
 				'service' => $service,
-				'loginName' => $loginName,
-				'loginPass' => $loginPass,
+				'loginName' => $login_name,
+				'loginPass' => $login_pass,
 				'enabled' => 'Y',
+				'syncReply' => $options['sync_reply'],
+				'syncConference' => $options['sync_conference'],
 			);
-			if( $idExist ) {
-				JWDB::UpdateTableRow( 'BindOther', $idExist, $uArray );
-				return $idExist;
-			}else{
-				$uArray['timeCreate'] = date('Y-m-d H:i:s');
-				return JWDB::SaveTableRow( 'BindOther', $uArray );
+			if( $exist_id ) 
+			{
+				JWDB::UpdateTableRow( 'BindOther', $exist_id, $up_array );
+				return $exist_id;
+			}
+			else
+			{
+				$up_array['timeCreate'] = date('Y-m-d H:i:s');
+				return JWDB::SaveTableRow( 'BindOther', $up_array );
 			}
 		}
 
 		return false;
 	}
 
-	static public function Destroy( $idUser, $idBindOrder ) { 
-		$idBindOrder = JWDB::CheckInt( $idBindOrder );
-		$idUser = JWDB::CheckInt( $idUser );
-		$delArray = array(
-				'idUser' => $idUser,
-				'id' => $idBindOrder,
+	static public function Destroy( $user_id, $bindother_id ) 
+	{ 
+		$bindother_id = JWDB::CheckInt( $bindother_id );
+		$user_id = JWDB::CheckInt( $user_id );
+		$del_array = array(
+				'idUser' => $user_id,
+				'id' => $bindother_id,
 				);  
-		return JWDB::DelTableRow( 'BindOther', $delArray );
+		return JWDB::DelTableRow( 'BindOther', $del_array );
 	}   
 
-	static public function Disable( $idBindOrder ) {
-		$idBindOrder = JWDB::CheckInt( $idBindOrder );
-		$uArray = array(
+	static public function Disable( $bindother_id ) 
+	{
+		$bindother_id = JWDB::CheckInt( $bindother_id );
+		$up_array = array(
 			'enabled' => 'N',
 		);
-		return JWDB::UpdateTableRow('BindOther', $idBindOrder, $uArray);
+		return JWDB::UpdateTableRow('BindOther', $bindother_id, $up_array);
 	}
 
-	static public function GetBindOther( $idUser ) 
+	static public function GetBindOther( $user_id ) 
 	{
-		$idUser = JWDB::CheckInt( $idUser );
-		$sql = "SELECT * FROM BindOther WHERE idUser=$idUser";
+		$user_id = JWDB::CheckInt( $user_id );
+		$sql = "SELECT * FROM BindOther WHERE idUser=$user_id";
 		$r = JWDB::GetQueryResult( $sql, true );
 		if( empty($r) ) 
 			return array();
@@ -92,23 +107,23 @@ class JWBindOther {
 		return $rtn;
 	}
 
-	static public function CheckTwitter( $loginName='name', $loginPass='123456' ) 
+	static public function CheckTwitter( $login_name='name', $login_pass='123456' ) 
 	{
-		return self::CheckAccount( $loginName, $loginPass, self::AUTH_TWITTER );
+		return self::CheckAccount( $login_name, $login_pass, self::AUTH_TWITTER );
 	}
 
-	static public function CheckFanfou( $loginName='name', $loginPass='123456' ) 
+	static public function CheckFanfou( $login_name='name', $login_pass='123456' ) 
 	{
-		return self::CheckAccount( $loginName, $loginPass, self::AUTH_FANFOU );
+		return self::CheckAccount( $login_name, $login_pass, self::AUTH_FANFOU );
 	}
 
-	static public function CheckAccount( $loginName='name', $loginPass='123456', $checkUrl=self::AUTH_TWITTER ) 
+	static public function CheckAccount( $login_name='name', $login_pass='123456', $checkUrl=self::AUTH_TWITTER ) 
 	{
-		$authCode = Base64_Encode( "$loginName:$loginPass" );
+		$auth_code = Base64_Encode( "$login_name:$login_pass" );
 
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, $checkUrl);  
-		curl_setopt($ch, CURLOPT_HTTPHEADER, array( "Authorization: Basic $authCode" ) );
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array( "Authorization: Basic $auth_code" ) );
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_TIMEOUT, 0.010); 
 		curl_exec($ch);
@@ -118,49 +133,49 @@ class JWBindOther {
 		return ( $httpCode == 200 );
 	}
 
-	static public function PostStatus( $bindOther=array(), $message=null ) 
+	static public function PostStatus( $bind_other_row=array(), $message=null ) 
 	{
-		if( empty( $bindOther ) || false == isset($bindOther['service']) )
+		if( empty( $bind_other_row ) || false == isset($bind_other_row['service']) )
 			return true;
 
-		if( $bindOther['enabled'] == 'N' )
+		if( $bind_other_row['enabled'] == 'N' )
 			return true;
 
-		$service = $bindOther['service'];
-		$loginPass = $bindOther['loginPass'];
-		$loginName = $bindOther['loginName'];
+		$service = $bind_other_row['service'];
+		$login_pass = $bind_other_row['loginPass'];
+		$login_name = $bind_other_row['loginName'];
 
 		switch( $service ) {
 			case 'twitter':
-				self::PostTwitter( $loginName, $loginPass, $message );
+				self::PostTwitter( $login_name, $login_pass, $message );
 			break;
 			case 'fanfou':
-				self::PostFanfou( $loginName, $loginPass, $message );
+				self::PostFanfou( $login_name, $login_pass, $message );
 			break;
 		}
 		return true;
 	}
 
-	static public function PostTwitter( $loginName='name', $loginPass='123456', $message=null ){
-		$postData = 'source=jiwai&status='.urlEncode( $message );
-		return self::RealPostStatus( $loginName, $loginPass, $postData, self::POST_TWITTER );
+	static public function PostTwitter( $login_name='name', $login_pass='123456', $message=null ){
+		$post_data = 'source=jiwai&status='.urlEncode( $message );
+		return self::RealPostStatus( $login_name, $login_pass, $post_data, self::POST_TWITTER );
 	}
 
-	static public function PostFanfou( $loginName='name', $loginPass='123456', $message=null ){
-		$postData = 'status='.urlEncode( $message );
-		return self::RealPostStatus( $loginName, $loginPass, $postData, self::POST_FANFOU );
+	static public function PostFanfou( $login_name='name', $login_pass='123456', $message=null ){
+		$post_data = 'status='.urlEncode( $message );
+		return self::RealPostStatus( $login_name, $login_pass, $post_data, self::POST_FANFOU );
 	}
 
-	static public function RealPostStatus( $loginName='name', $loginPass='123456', $postData=null, $postUrl=null ) 
+	static public function RealPostStatus( $login_name='name', $login_pass='123456', $post_data=null, $post_url=null ) 
 	{
-		$authCode = Base64_Encode( "$loginName:$loginPass" );
+		$auth_code = Base64_Encode( "$login_name:$login_pass" );
 
 		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $postUrl);  
-		curl_setopt($ch, CURLOPT_HTTPHEADER, array( "Authorization: Basic $authCode" ) );
+		curl_setopt($ch, CURLOPT_URL, $post_url);  
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array( "Authorization: Basic $auth_code" ) );
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_POST, true);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
 		curl_setopt($ch, CURLOPT_TIMEOUT, 0.010); 
 		curl_exec($ch);
 		curl_close($ch);
