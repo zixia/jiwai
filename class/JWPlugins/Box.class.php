87baa302f4a5cfedcc9536d420612fedbdcd2179
Shuwen DAI <shwdai@gmail.com>
2007-12-18 03:00:08 +0000

    see #262; (?:www\.|)8box.cn

--- class/JWPlugins/Box.class.php
+++ class/JWPlugins/Box.class.php
@@ -16,33 +16,33 @@ class JWPlugins_Box
 		return null;
 	}
 
-    static public function GetBoxId( $string )
-    {
-        if( preg_match('#http://www\.8box\.c[n|om]([\/a-z\/]+)+([0-9]+)#i', $string, $matches))
-        {
-            return $matches[2];
-        }
-        else
-        {
-            return false;
-        }
+	static public function GetBoxId( $string )
+	{
+		if( preg_match('#http://(?:www\.|)8box\.c[n|om]([\/a-z\/]+)+([0-9]+)#i', $string, $matches))
+		{
+			return $matches[2];
+		}
+		else
+		{
+			return false;
+		}
 
-    }
-    
-    static public function GetPluginInfo( $string )
-    {
-	    if( $box_id = self::GetBoxId( $string ) )
-	    {
-		    return self::BuildBoxUrl( $box_id );
-	    }
+	}
 
-	    return false;
-    }
+	static public function GetPluginInfo( $string )
+	{
+		if( $box_id = self::GetBoxId( $string ) )
+		{
+			return self::BuildBoxUrl( $box_id );
+		}
 
-    static public function BuildBoxUrl( $box_id )
-    {
-        return 'http://www.8box.cn/feed/e92400_'.$box_id.'/p.swf';
-    }
+		return false;
+	}
+
+	static public function BuildBoxUrl( $box_id )
+	{
+		return 'http://www.8box.cn/feed/e92400_'.$box_id.'/p.swf';
+	}
 
 }
 ?>
