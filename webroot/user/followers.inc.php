338a7ef95aa06b6fcf62c151fc1c011030857ecb
Shuwen DAI <shwdai@gmail.com>
2008-01-29 06:37:09 +0000

    fix #334 author:wqsemc

--- webroot/user/followers.inc.php
+++ webroot/user/followers.inc.php
@@ -4,6 +4,15 @@ function user_friends($idUser)
 	$g_user_friends = true;
 	$g_page_user_id = $idUser;
 
-	include(dirname(__FILE__).'/../wo/followers/index.php');
+	$logined_user_info = JWUser::GetCurrentUserInfo(); 
+
+	if ( $logined_user_info && $logined_user_info['id']==$g_page_user_id )
+	{
+		include(dirname(__FILE__).'/../wo/followers/index.php');
+	}
+	else
+	{
+		JWTemplate::RedirectTo404NotFound();
+	}
 }  // end function
 ?>
