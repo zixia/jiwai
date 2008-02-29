addaafc720a19742ce26f764ceff0a9b7d3eadfb
Song DU <freewizard@gmail.com>
2008-02-29 16:07:18 +0000

    see #393, tag, reply & mms in UWA

--- domain/api/uwa/index.php
+++ domain/api/uwa/index.php
@@ -3,12 +3,14 @@ echo '<?xml version="1.0" encoding="UTF-8" ?>';
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xmlns:widget="http://www.netvibes.com/ns/">
-<head><title>叽歪</title>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>叽歪</title>
 <link rel="icon" type="image/x-icon" href="http://asset.jiwai.de/img/favicon.ico" />
 <meta name="author" content="叽歪" />
 <meta name="website" content="http://jiwai.de" />
-<meta name="description" content="叽歪窗可贴 for UWA (Netvibes, SOW and etc)" />
-<meta name="version" content="1.0.1" />
+<meta name="description" content="叽歪窗可贴 for UWA (Netvibes, Sohu Blog and etc)" />
+<meta name="version" content="1.1.1" />
 <meta name="keyword" content="叽歪 JiWai widget" />
 <meta name="screenshot" content="http://asset.jiwai.de/gadget/uwa/sow_screenshot.png" />
 <meta name="thumbnail" content="http://asset.jiwai.de/gadget/uwa/sow_thumbnail.png" />
@@ -18,29 +20,41 @@ echo '<?xml version="1.0" encoding="UTF-8" ?>';
 	<preference type="text" label="你的叽歪登录名" name="screenName" />
 	<preference type="boolean" label="显示好友的更新" name="withFriends" defaultValue="true" />
 	<preference type="range" label="最多显示" name="numDisplay" min="1" max="20" step="1" defaultValue="5" />
+	<preference type="boolean" label="显示留言框" name="updateBox" defaultValue="true" />
 </widget:preferences>
 <script type="text/javascript">
 <![CDATA[
+function formatStatus(s) {
+	return s.replace(/\[(.+)\]/, '[<a target="_blank" href="http://jiwai.de/t/$1/">$1</a>]');
+}
 function formatDate(s) {
 	var d = new Date(s.replace(/(\w+)\s+(\w+)\s+(\w+)\s+([\w:]+)\s+([\w+-]+)\s+(\w+)/, '$1 $2 $3 $6 $4 $5'));
 	return (d.toLocaleDateString() == (new Date).toLocaleDateString()) 
 		? d.toLocaleTimeString().replace(/(\d+):(\d+):(\d+)/, '$1:$2') 
 		: d.toLocaleDateString();
 }
+var jw_footer = '';
 widget.onLoad = function() {
 	var screenName = widget.getValue("screenName");
 	var withFriends = widget.getValue("withFriends") == 'true';
 	var numDisplay = widget.getValue("numDisplay");
+	var updateBox = widget.getValue("updateBox") == 'true';
+	var uri;
 	if (numDisplay<1) numDisplay = 1;
 	if (numDisplay>20) numDisplay = 20;
 	if (screenName == null || screenName == '') {
-		widget.setBody('<p class="alert">请先设置你的叽歪登录名<br />还没有叽歪帐号？<a href="http://jiwai.de/wo/account/create" target="_blank">这里注册</a></p>');
-		return;
+		jw_footer = '<p class="alert">现在显示的是叽歪广场。设置你的叽歪帐号后就可以在这里显示你和好友的最新消息了。还没有叽歪帐号？<a href="http://jiwai.de/wo/account/create" target="_blank">这里注册</a></p>'
+		widget.setBody(jw_footer);
+		uri = 'http://api.jiwai.de/statuses/public_timeline.json?count=5';
+		withFriends = true;
+	} else {
+		widget.setTitle(screenName+'的叽歪');
+		uri = 'http://api.jiwai.de/statuses/'+(withFriends ? 'friends' : 'user')+'_timeline/' + screenName + '.json?count='+numDisplay;
+		jw_footer = updateBox ? '<form style="text-align:center;" action="http://jiwai.de/wo/status/update" method="post" onsubmit="var s=document.getElementById(\'jw_status\'); if (s.value==\'\') return false; else s.value=\'@'+screenName+' \'+s.value;"><textarea id="jw_status" name="jw_status" rows="3"></textarea><br /><input style="border:1px;" type="submit" value="留言"/><a target="_blank" href="http://jiwai.de/wo/account/create">注册叽歪</a></form>' : '';
 	}
-	var uri = 'http://api.jiwai.de/statuses/'+(withFriends ? 'friends' : 'user')+'_timeline/' + screenName + '.json?count='+numDisplay;
 	UWA.Data.getJson(uri, function(response){
 		if (response == null) {
-			widget.setBody('<p class="alert">Invalid data.</p>');
+			widget.setBody('<p class="alert">无效数据</p>');
 			return null;
 		}
 		var html = '';
@@ -49,24 +63,27 @@ widget.onLoad = function() {
 			var o = response[i];
 			l++;
 			try{
-				html += '<div style="padding-bottom:4px;margin-top:4px;border-bottom:solid 1px #eee;">'
-				+ '<span><img style="float:right;width:24px;height:24px;" src="'+o.user.profile_image_url //align="right"
-				+ (withFriends ? '"/></span><span>' + o.user.screen_name + ': ' : '"/>')
-				+ '</span><span>' + o.text
-				+ '</span> <span style="color:grey">' + formatDate(o.created_at)
-				+ '</span></div>';
+				html += '<div style="padding-bottom:4px;margin-top:4px;border-bottom:solid 1px #eee;position:relative;" onmouseover="UWA.$element(document.getElementById(\'r_' + o.id + '\')).show();" onmouseout="UWA.$element(document.getElementById(\'r_' + o.id + '\')).hide();">'
+				+ '<span><a target="_blank" href="'
+				+ o.user.profile_url + '"><img style="float:right;width:24px;height:24px;" src="'
+				+ o.user.profile_image_url + '"/></a>' //align="right"
+				+ (withFriends ? '</span><span><a target="_blank" href="'+o.user.profile_url+'">' + o.user.screen_name + '</a>: ' : '')
+				+ '</span><span>' + formatStatus(o.text)
+				+ '</span> <span style="color:lightgrey">' + formatDate(o.created_at)
+				+ '</span>' + (o.mms_image_url ? '<br /><a href="'+o.user.profile_url+'mms/'+o.id+'" target="_blank"><img src="'+o.mms_image_url+'" width="120" align="bottom"/></a>' : '')
+				+ '<span id="r_' + o.id + '" style="position:absolute;right:0px;display:none;"><a style="background:#eee" target="_blank" href="'+o.user.profile_url+'thread/'+o.id+'">回复</a></span></div>';
 			} catch(e) {
 				//just ignore exceptions since some crap func stay in the object.
 			}
 		}
-		widget.setBody(html);
+		widget.setBody(html+jw_footer);
 	});
 };
 widget.onRefresh = widget.onLoad;
 ]]></script>
 </head>
 <body>
-<div id="content"><p class="alert">Please wait...</p></div>
+<div id="content"><p class="alert">正在加载……</p></div>
 </body>
 </html>
 
