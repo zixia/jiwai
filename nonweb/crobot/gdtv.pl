34b27e6234f0bb4f70afd49913b42168b27ccbf6
Hongwei WANG <glinus@gmail.com>
2008-03-02 04:40:25 +0000

    see #331
    == more bots ==
     * GDTV
     * NBA (http://china.nba.com/)

---
+++ nonweb/crobot/gdtv.pl
@@ -0,0 +1,161 @@
+#!/usr/bin/perl -w
+
+use strict;
+use Data::Dumper;
+use Text::Iconv;
+
+my %channelMap = (
+    'GDTV1'    => '广东卫视',
+    'GDTV2'    => '广东电视台珠江频道',
+    'GDTV3'    => '广东电视台体育频道',
+    'GDTV4'    => '广东电视台公共频道',
+    'GDTV5'    => '广东电视台珠江频道海外版',
+    'GDTV6'    => '广东电视台新闻频道',
+    'GDTV7'    => '嘉佳卡通频道',
+);
+
+my %userMap = (
+    'GDTV1'    => 'gdtv1',
+    'GDTV2'    => 'gdtv2',
+    'GDTV3'    => 'gdtv3',
+    'GDTV4'    => 'gdtv4',
+    'GDTV5'    => 'gdtv5',
+    'GDTV6'    => 'gdtv6',
+    'GDTV7'    => 'gdtv7',
+);
+
+sub getTVGuideCacheByChannel {
+    my $channel = shift;
+    my $timestamp = `date +%Y%m%d -d "6 hours ago"`; chomp $timestamp;
+    return "/tmp/gdtv/$channel.$timestamp.cache";
+}
+
+sub getTVGuideByChannel {
+    my $channel = shift;
+    my $url = getTVGuideUrlByChannel($channel);
+    my @guide = ();
+    my $raw = `wget -U "Googlebot" -q -O - $url`;
+    my $today = `date +%Y-%m-%d`; chomp $today;
+    my $tomorrow = `date +%Y-%m-%d -d tomorrow`; chomp $tomorrow;
+
+    my $converter = Text::Iconv->new("gbk", "utf-8");
+    my $converted = $converter->convert($raw);
+
+    open HTTP, "<", \$converted;
+    my ($roi, $across, $hourSoFar, $hourNow) = (0, 0, 0, 0);
+
+    while (<HTTP>) {
+        my ($time, $show) = ();
+        chomp;
+        if (m#<div\s+id="pg">#i) {$roi = 1;}
+        next if ($roi eq 0);
+        $_ =~ s#<a\s+href=.*?>##gi;
+        $_ =~ s#<\/a>##gi;
+        $_ =~ s#<img\s+.*?>##gi;
+        $_ =~ s#<div\s+style.*?>##gi;
+        if (m#<div\s+id="pgrow">.*?<font.*?>([^<> ]+)<\/font>.*?([^<>]*?)\s+<\/div>#i) {
+            ($time, $show) = ($1, $2);
+            ($hourNow) = split(":", $1);
+        } elsif (m#<div\s+id="pgrow">.*?<font.*?>([^<> ]+)<\/font>\s+<div.*?>([^<>]*?)\s+#i) {
+            ($time, $show) = ($1, $2);
+            ($hourNow) = split(":", $1);
+        }
+        if (($across eq 0) and ($hourNow < $hourSoFar)) {
+            $across = 1;
+        }
+        next unless defined $time;
+        $hourSoFar = $hourNow;
+        if ($across eq 1) {
+            push(@guide, "$tomorrow $time;$show;0");
+        } else {
+            push(@guide, "$today $time;$show;0");
+        }
+    }
+
+    close HTTP;
+    return @guide;
+}
+
+sub getTVGuideUrlByChannel {
+    my $channel = shift;
+    die "no channel specified" unless defined $channel;
+
+    ##http://www.tvmao.com/program/GDTV-GDTV1-w4.html
+    my $retstr = 'http://www.tvmao.com/program/GDTV-0Channel0-w0indexOfWeek0.html';
+    my $indexOfWeek = `date +%u`; chomp $indexOfWeek;
+    $indexOfWeek = 7 if ($indexOfWeek eq 0);
+
+    $retstr =~ s/0Channel0/$channel/si;
+    $retstr =~ s/0indexOfWeek0/$indexOfWeek/si;
+
+    return $retstr;
+}
+
+sub TVGuideFactory {
+    my $channel = shift;
+    die "no channel specified" unless defined $channel;
+
+    my @tvguide = getTVGuideByChannel($channel);
+    return @tvguide;
+}
+
+sub writeCache {
+    my ($channel, @guide) = @_;
+
+    my $cache = getTVGuideCacheByChannel($channel);
+
+    open CACHE, ">$cache" or warn "$cache: $!";
+    for my $entry (@guide) {
+        chomp $entry;
+        print CACHE $entry, "\n";
+    }
+    close CACHE
+}
+
+sub postTVGuide {
+    my ($channel) = @_;
+    die "no channel specified" unless defined $channel;
+
+    my ($username, $password) = ($userMap{$channel}, $userMap{$channel} . 'epgdem1ma');
+
+    my $cache = getTVGuideCacheByChannel($channel);
+
+    open CACHE, "<$cache" or warn "$cache: $!";
+    my @guide = <CACHE>;
+    close CACHE;
+
+    if (!@guide) {
+        @guide = TVGuideFactory($channel);
+        writeCache($channel, @guide);
+    }
+
+    my ($lower, $upper) = (60 * 15, 60 * 30);
+    my $len = @guide;
+    for my $i (0 .. $len - 1) {
+        my $entry = $guide[$i]; chomp $entry;
+        my ($time, $show, $f) = split(/;/, $entry);
+        $show =~ s#^\s+##gi;
+        my $tsNow = `date +%s`; chomp $tsNow;
+        my $tsShow= `date +%s -d "$time"`; chomp $tsShow;
+        my $tsDiff = int($tsShow - $tsNow);
+        if ($tsDiff > $lower and $tsDiff < $upper and $f eq 0) {
+            print "$channel $time $show $tsDiff\n";
+            `curl -s -u "$username:$password" -Fstatus="$channelMap{$channel} $time $show" http://api.jiwai.de/statuses/update.json`;
+            $guide[$i] = "$time;$show;1";
+        }
+    }
+    writeCache($channel, @guide);
+}
+
+sub createAccountByChannel {
+    my $channel = shift;
+    die "no channel specified" unless defined $channel;
+
+    my ($username, $password) = ($userMap{$channel}, $userMap{$channel} . 'epgdem1ma');
+
+    `curl -s -Fname_screen=$username -Fpass=$password -Femail=$username\@jiwai.de -Fapikey=4107f1349979cc9ed2951fb82b6105d4 http://api.jiwai.de/account/new.json`;
+}
+
+for my $channel (keys %channelMap) {
+    postTVGuide($channel);
+}
---
+++ nonweb/crobot/nba.pl
@@ -0,0 +1,205 @@
+#!/usr/bin/perl -w
+
+use strict;
+use Data::Dumper;
+
+my %teamMap = (
+    '波士顿凯尔特人'=> {
+        'id'    => 'celtics',
+    },
+    '新泽西网'      => {
+        'id'    => 'nets',
+    },
+    '纽约尼克斯'    => {
+        'id'    => 'knicks',
+    },
+    '费城76人'      => {
+        'id'    => '76ers',
+    },
+    '多伦多猛龙'    => {
+        'id'    => 'raptors',
+    },
+    '芝加哥公牛'    => {
+        'id'    => 'bulls',
+    },
+    '克里夫兰骑士'  => {
+        'id'    => 'cavaliers',
+    },
+    '底特律活塞'    => {
+        'id'    => 'pistons',
+    },
+    '印第安纳步行者'=> {
+        'id'    => 'pacers',
+    },
+    '密尔沃基雄鹿'  => {
+        'id'    => 'bucks',
+    },
+    '亚特兰大鹰'    => {
+        'id'    => 'hawks',
+    },
+    '夏洛特山猫'    => {
+        'id'    => 'bobcats',
+    },
+    '迈阿密热'      => {
+        'id'    => 'heat',
+    },
+    '奥兰多魔术'    => {
+        'id'    => 'magic',
+    },
+    '华盛顿奇才'    => {
+        'id'    => 'wizards',
+    },
+    '达拉斯小牛'    => {
+        'id'    => 'mavericks',
+    },
+    '休斯敦火箭'    => {
+        'id'    => 'rockets',
+    },
+    '孟菲斯灰熊'    => {
+        'id'    => 'grizzlies',
+    },
+    '新奥尔良黄蜂'  => {
+        'id'    => 'hornets',
+    },
+    '圣安东尼奥马刺'=> {
+        'id'    => 'spurs',
+    },
+    '丹佛掘金'      => {
+        'id'    => 'nuggets',
+    },
+    '明尼苏达森林狼'=> {
+        'id'    => 'timberwolves',
+    },
+    '波特兰开拓者'  => {
+        'id'    => 'blazers',
+    },
+    '西雅图超音速'  => {
+        'id'    => 'supersonics',
+    },
+    '犹他爵士'      => {
+        'id'    => 'jazz',
+    },
+    '金州勇士'      => {
+        'id'    => 'warriors',
+    },
+    '洛杉矶快船'    => {
+        'id'    => 'clippers',
+    },
+    '洛杉矶湖人'    => {
+        'id'    => 'lakers',
+    },
+    '菲尼克斯太阳'  => {
+        'id'    => 'suns',
+    },
+    '萨克拉门托国王'=> {
+        'id'    => 'kings',
+    },
+);
+
+sub urlFactory {
+    my ($key, $options) = @_;
+    my %options = %$options;
+
+    return 'http://china.nba.com/'. $options{$key}{'id'} .'/';
+}
+
+sub convertToAbsUrl {
+    my ($relative) = @_;
+    return 'http://china.nba.com' . $relative;
+}
+
+sub getItems {
+    my ($url) = @_;
+    my ($roiBegin, $roiEnd) = ('-球队新闻', '-\/球队新闻');
+
+    my $withRoi = 0;
+    my %items = ();
+
+    open PAGE, "wget -A 'Googlebot' -q $url -O - |iconv -f gbk -t utf-8 |" or die "$!";
+
+    while (<PAGE>) {
+        chomp;
+        if (m/$roiBegin/) {
+            $withRoi = 1;
+        } elsif (m/$roiEnd/) {
+            $withRoi = 0;
+        }
+        next if $withRoi eq 0;
+
+        my ($roi, $pattern) = ($_, qr/<a\s+href=\"(.*?)\"\s+target=\"_blank\">([^<]+)/);
+        while ($roi =~ m/$pattern/) {
+            my $link = $1;
+            $link =~ s/,/，/gi;
+            $items{$link} = $2;
+            $roi =~ s/$pattern//i;
+        }
+    }
+
+    close PAGE;
+
+    return %items;
+}
+
+sub _urlencode {
+    my $text = shift;
+    $text =~ s/([^A-Za-z0-9])/sprintf("%%%02X", ord($1))/seg;
+    return $text;
+}
+
+sub _postItem {
+    my ($user, $pass, $text) = @_;
+    $text = _urlencode($text);
+    my $err = `curl -s -u "$user:$pass" -Fstatus="$text" http://api.jiwai.de/statuses/update.json`;
+}
+
+sub _postStrFactory {
+    my ($t, $l) = @_;
+    my $str = "";
+    open FH, ">", \$str;
+    print FH $t," ",$l;
+    close FH;
+    return $str;
+}
+
+sub _loadCache {
+    my ($cache) = @_;
+    my %cachedItems = ();
+
+    open FH, "<$cache" or return;
+
+    while (<FH>) {
+        chomp;
+        my ($title, @link) = split(",", $_);
+        my $link = join(',', @link);
+        $cachedItems{$link} = $title;
+    }
+
+    close FH;
+
+    return %cachedItems;
+}
+
+my $cache = '/tmp/feed/nba.cache';
+my $sleep = 10;
+my %cachedItems = _loadCache($cache);
+open CACHE, ">>$cache" or die "failed write: $!";
+
+foreach my $key (keys %teamMap) {
+    my $url = urlFactory($key, \%teamMap);
+    my %items = getItems($url);
+    while (my($link, $title) = each %items) {
+        my $str = _postStrFactory($title, convertToAbsUrl($link));
+        if (defined $cachedItems{$link}) {
+            print "[DUP]", $str, "\n";
+        } elsif (defined $ENV{'RSS_NONPOST'} and $ENV{'RSS_NONPOST'} eq 1) {
+            print "[HLD]", $str, "\n";
+        } else {
+            print CACHE $title,",",$link,"\n";
+            _postItem($key, 'sportdem1ma', $str);
+            print "[INF]", $str, "\n";
+        }
+    }
+    sleep $sleep;
+}
+
+close CACHE;
