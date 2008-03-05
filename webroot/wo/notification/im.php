d9890120c62c6cea2d6f9f71fb78f9829a4346a2
Qiang WANG <wqsemc@gmail.com>
2008-03-05 10:10:33 +0000

    See #379, By WqSemc, support not notify user when they in bed

--- webroot/wo/notification/im.php
+++ webroot/wo/notification/im.php
@@ -8,12 +8,17 @@
  $user_info      = JWUser::GetCurrentUserInfo();
  
  $user_setting   = JWUser::GetNotification($user_info['id']);
- 
+
  if ( isset($_REQUEST['commit_x']) )
  {
 	$user_new_setting = isset($_POST['user']) ? $_POST['user'] : array();
 	$user_setting['auto_nudge_me'] = isset($user_new_setting['auto_nudge_me']) ? $user_new_setting['auto_nudge_me'] : 'N';
 	$user_setting['is_receive_offline'] = isset($user_new_setting['is_receive_offline']) ? $user_new_setting['is_receive_offline'] : 'N';
+	$user_setting['allowSystemSms'] = !empty($user_new_setting['allowSystemSms']) ? 'Y' : 'N';
+	$user_setting['isNotReceiveNight'] = !empty($user_new_setting['isNotReceiveNight']) ? 'Y' : 'N';
+	$user_setting['notReceiveTime1'] = $user_new_setting['notReceiveTime1'];
+	$user_setting['notReceiveTime2'] = $user_new_setting['notReceiveTime2'];
+	$user_setting['allowReplyType'] = $user_new_setting['allowReplyType'];
  
 	if ( ! JWUser::SetNotification($user_info['id'], $user_setting) )
 	{
@@ -66,16 +71,36 @@ JWTemplate::html_head(array(
 <p class="right14"><a href="/wo/notification/email">Email</a>&nbsp;|&nbsp;&nbsp;<a href="/wo/notification/im" class="now">手机/聊天软件</a></p>
         <div class="binding">
          <p>
-             <input <?php if ( 'Y'==$user_setting['auto_nudge_me'] ) echo ' checked="checked" ';?> 
-                     id="user_auto_nudge_me" name="user[auto_nudge_me]" type="checkbox" value="Y" />
-             <label for="user_auto_nudge_me">如果我在24小时内没有更新，请让叽歪小弟提醒我</label>
+             <label for="user_auto_nudge_me"><input <?php if ( 'Y'==$user_setting['auto_nudge_me'] ) echo ' checked';?> 
+                     id="user_auto_nudge_me" name="user[auto_nudge_me]" type="checkbox" value="Y" />&nbsp;如果我在24小时内没有更新，请让叽歪小弟提醒我</label>
          </p>
          <p>
-             <input <?php if ( 'Y'==$user_setting['is_receive_offline'] ) echo ' checked="checked" ';?> 
-                     id="is_receive_offline" name="user[is_receive_offline]" type="checkbox" value="Y" />
-             <label for="is_receive_offline">当我的聊天软件处于离线状态时也给我发送通知</label>
+             <label for="is_receive_offline"><input <?php if ( 'Y'==$user_setting['is_receive_offline'] ) echo ' checked';?> 
+                     id="is_receive_offline" name="user[is_receive_offline]" type="checkbox" value="Y" />&nbsp;当我的聊天软件处于离线状态时也给我发送通知</label>
          </p>
-	   <p class="checkboxText">我们支持qq的隐身，msn的脱机</p>		 
+	   <p class="checkboxText">我们支持qq的隐身，msn的脱机</p>	
+	   <p><label for="user_isNotReceiveNight"><input type="checkbox" id="user_isNotReceiveNight" name="user[isNotReceiveNight]" value="Y"<?php if ( 'Y'==$user_setting['isNotReceiveNight'] ) echo ' checked';?> />&nbsp;睡觉时间不要给我通知</label> 
+	     <select name="user[notReceiveTime1]" size="1" class="select Width80">
+	     <? for($i=0;$i<24;$i++){
+		     $time_array = explode(":", $user_setting['notReceiveTime1'], 3);
+		?>
+	     <option value="<? echo $i>9? $i : "0$i";?>:00:00"<? echo $i != intval($time_array[0])? '' : ' selected';?>><? echo $i>9? $i : "0$i";?>:00</option>
+	     <? } ?>
+		 </select> 至<select name="user[notReceiveTime2]" size="1" class="select Width80">
+	     <? for($i=0;$i<24;$i++){
+		     $time_array = explode(":", $user_setting['notReceiveTime2'], 3);
+		?>
+	     <option value="<? echo $i>9? $i : "0$i";?>:00:00"<? echo $i != intval($time_array[0])? '' : ' selected';?>><? echo $i>9? $i : "0$i";?>:00</option>
+	     <? } ?>
+		 </select></p>	
+        <p><label for="user_allowSystemSms"><input type="checkbox" id="user_allowSystemSms" name="user[allowSystemSms]" value="Y"<?php if ( 'Y'==$user_setting['allowSystemSms'] ) echo ' checked';?>/>&nbsp;允许叽歪通过短信联系我</label></p>
+        <p class="sysUpdate">我想<select id="user_allowReplyType" name="user[allowReplyType]" size="1" class="select seWidth">
+          <option value="everyone"<?php if ( 'everyone'==$user_setting['allowReplyType'] ) echo ' selected ';?>>接收我关注的人给任何人的回复</option> 
+          <option value="each"<?php if ( 'each'==$user_setting['allowReplyType'] ) echo ' selected ';?>>接收我关注的人们相互之间的回复</option>        
+          <option value="mine"<?php if ( 'mine'==$user_setting['allowReplyType'] ) echo ' selected ';?>>只接收给我的回复</option>        
+          <option value="none"<?php if ( 'none'==$user_setting['allowReplyType'] ) echo ' selected ';?>>不接收任何回复</option>
+         </select>
+          <a href="http://help.jiwai.de/ReplyReceive" target="_blank" class="orange12">什么意思？</a></p> 
 	   <p class="sysUpdate"><input type="submit" id="save" name="save" class="submitbutton" value="保存" /></p>
 	   </div>
 	   <div style="overflow: hidden; clear: both; height: 50px; line-height: 1px; font-size: 1px;"></div>
