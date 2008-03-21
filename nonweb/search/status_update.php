a42221a937e67a388ea7a773077f6995b85caa8b
Shuwen DAI <shwdai@gmail.com>
2008-03-21 15:12:46 +0000

    see #425 output infomation for update index via web call

--- nonweb/search/status_update.php
+++ nonweb/search/status_update.php
@@ -1,43 +1,54 @@
 <?php
 import de.jiwai.lucene.*;
 import de.jiwai.dao.*;
-
+error_reporting(0);
 $status_index = '/opt/lucene/index/status';
-
-echo "<pre>";
 
 $id = intval($_GET['id']);
 
 if ( $id )
 {
 	$record = Execute::GetOnePK( 'Status', $id );
+	$key_field = "id";
+	$key_value = $id;
+
+	$lucene_index = new LuceneIndex( $status_index );
+
 	if ( $record ) 
 	{
-
 		/** 
 		 * need utf8-encode for display;,
 		 * for lucene index , no need any operation
 		 */
-		$lucene_index = new LuceneIndex( $status_index );
-		$key_field = "id";
-		$key_value = $record->get("id");
 
-		$token = array( true );
-		$other_field = array("status");
-		$other_value = array(
-			$record->get("status"),
-		);
 
-		$lucene_index->update( $key_field, $key_value, $other_field, $other_value, $token );
-		//$lucene_index->delete( $key_field, $key_value );
-		$lucene_index->flush();
-		$lucene_index->close();
+		$token = array( true, );
+		$other_field = array( "status", );
+		$other_value = array( $record->get("status"), );
 
-		echo '{"error":0}';
-		exit;
+		try {
+			$lucene_index->update( $key_field, $key_value, $other_field, $other_value, $token );
+			$lucene_index->flush();
+			$lucene_index->close();
+		}catch(Execute $e){
+			die( '{"error":1,"action":"update"}' );
+		}
+
+		die ('{"error":0,"action":"update"}') ;
+	}
+	else
+	{
+		try {
+			$lucene_index->delete( $key_field, $key_value );
+			$lucene_index->flush();
+			$lucene_index->close();
+		}catch(Execute $e){
+			die( '{"error":1,"action":"delete"}' );
+		}
+
+		die ('{"error":0,"action":"delete"}') ;
 	}
 }
 
-echo '{"error":1}';
-exit;
+die ('{"error":1,"action":"none"}') ;
 ?>
--- nonweb/search/user_update.php
+++ nonweb/search/user_update.php
@@ -1,26 +1,25 @@
 <?php
 import de.jiwai.lucene.*;
 import de.jiwai.dao.*;
-
+error_reporting(0);
 $user_index = '/opt/lucene/index/user';
-
-echo "<pre>";
 
 $id = intval($_GET['id']);
 
 if ( $id )
 {
 	$record = Execute::GetOnePK( 'User', $id );
+	$key_field = "id";
+	$key_value = $id;
+
+	$lucene_index = new LuceneIndex( $user_index );
+
 	if ( $record ) 
 	{
-
 		/** 
 		 * need utf8-encode for display;,
 		 * for lucene index , no need any operation
 		 */
-		$lucene_index = new LuceneIndex( $user_index );
-		$key_field = "id";
-		$key_value = $record->get("id");
 
 		$token = array( false, false, false, false, true );
 		$other_field = array("nameScreen", "email", "birthday", "gender", "bio");
@@ -32,21 +31,29 @@ if ( $id )
 			$record->get("bio"),
 		);
 
-		var_dump( $other_field );
-		var_dump( $other_value );
-		var_dump( $key_value );
-		var_dump( $token );
+		try {
+			$lucene_index->update( $key_field, $key_value, $other_field, $other_value, $token );
+			$lucene_index->flush();
+			$lucene_index->close();
+		}catch(Execute $e){
+			die( '{"error":1,"action":"update"}' );
+		}
 
-		$lucene_index->update( $key_field, $key_value, $other_field, $other_value, $token );
-		//$lucene_index->delete( $key_field, $key_value );
-		$lucene_index->flush();
-		$lucene_index->close();
+		die ('{"error":0,"action":"update"}') ;
+	}
+	else
+	{
+		try {
+			$lucene_index->delete( $key_field, $key_value );
+			$lucene_index->flush();
+			$lucene_index->close();
+		}catch(Execute $e){
+			die( '{"error":1,"action":"delete"}' );
+		}
 
-		echo '{"error":0}';
-		exit;
+		die ('{"error":0,"action":"delete"}') ;
 	}
 }
 
-echo '{"error":1}';
-exit;
+die ('{"error":1,"action":"none"}') ;
 ?>
