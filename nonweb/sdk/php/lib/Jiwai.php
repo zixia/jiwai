1f9566dbcda903372b077998bd75a397b74ea076
Song DU <freewizard@gmail.com>
2008-03-23 22:18:08 +0000

    see #429 php library

---
+++ nonweb/sdk/php/lib/Jiwai.php
@@ -0,0 +1,130 @@
+<?php
+require_once 'OAuth.php';
+
+define('JIWAI_API_HOST', 'api.beta.jiwai.de');
+define('JIWAI_OAUTH_REQUEST_TOKEN_URL', 'http://'.JIWAI_API_HOST.'/oauth/request_token');
+define('JIWAI_OAUTH_ACCESS_TOKEN_URL', 'http://'.JIWAI_API_HOST.'/oauth/access_token');
+define('JIWAI_OAUTH_AUTHORIZE_URL', 'http://beta.jiwai.de/wo/oauth/authorize');
+
+interface Jiwai_Auth {
+	function sendRequest($url, $param=array(), $method='GET');
+}
+
+class Jiwai_Auth_Basic implements Jiwai_Auth {
+	private $username;
+	private $password;
+	private $curl_handler;
+	protected function getCurlHandler() {
+		if (!$this->curl_handler) $this->curl_handler = curl_init();
+		return $this->curl_handler; 
+	}
+	function setLogin($username, $password) {
+		$this->username = $username;
+		$this->password = $password;
+	}
+	function sendRequest($url, $param=array(), $method='GET') {
+		if (strpos($url, '://')===false) $url = 'http://'.JIWAI_API_HOST.$url;
+		if (empty($param)) $param = array();
+		$curl = $this->getCurlHandler();
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curl, CURLOPT_HEADER, false);
+		curl_setopt($curl, CURLOPT_URL, $url);
+		curl_setopt($curl, CURLOPT_POST, strtoupper($method)=='POST');
+		curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($param));
+		if ($this->username) curl_setopt($curl, CURLOPT_USERPWD, $this->username.':'.$this->password);
+		$ret = curl_exec($curl);
+		if (!$ret) return false;
+		$code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+		if ($code!=200) return false;
+		return $ret;
+	}
+}
+
+class Jiwai_Auth_Oauth implements Jiwai_Auth {
+	private $consumer;
+	private $token;
+	private $sign_method;
+	private $curl_handler;
+	protected function getCurlHandler() {
+		if (!$this->curl_handler) $this->curl_handler = curl_init();
+		return $this->curl_handler; 
+	}
+	function __construct($consumer_key, $consumer_secret) {
+		$this->consumer = new OAuthConsumer($consumer_key, $consumer_secret);
+		$this->sign_method = new OAuthSignatureMethod_HMAC_SHA1();
+	}
+	function setToken($key, $secret = null) {
+		if ($secret) {
+			$this->token = new OAuthToken($key, $secret);
+		} else {
+			$this->token = $key;
+		}
+		return $this->token;
+	}
+	function getToken() {
+		return $this->token;
+	}
+	function fetchRequestToken() {
+		$this->setToken(null);
+		$ret = $this->sendRequest(JIWAI_OAUTH_REQUEST_TOKEN_URL);
+		if (!$ret) return false;
+		parse_str($ret, $t);
+		return $this->setToken($t['oauth_token'], $t['oauth_token_secret']);
+	}
+	function getAuthorizeUrl($callback = false) {
+		$url = JIWAI_OAUTH_AUTHORIZE_URL.'?oauth_token='.urlencode($this->token->key);
+		if ($callback) $url.= '&oauth_callback='.urlencode($callback);
+		return $url;
+	}
+	function fetchAccessToken() {
+		$ret = $this->sendRequest(JIWAI_OAUTH_ACCESS_TOKEN_URL);
+		if (!$ret) return false;
+		parse_str($ret, $t);
+		return $this->setToken($t['oauth_token'], $t['oauth_token_secret']);
+	}
+	function sendRequest($url, $param=array(), $method='GET') {
+		if (strpos($url, '://')===false) $url = 'http://'.JIWAI_API_HOST.$url;
+		if (empty($param)) $param = array();
+		$req = OAuthRequest::from_consumer_and_token($this->consumer, $this->token, strtoupper($method), $url, $param);
+		$req->sign_request($this->sign_method, $this->consumer, $this->token);
+		$curl = $this->getCurlHandler();
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curl, CURLOPT_HEADER, false);
+		if ($method=='GET') {
+			curl_setopt($curl, CURLOPT_POST, false);
+			curl_setopt($curl, CURLOPT_URL, $req->to_url());
+		} else {
+			curl_setopt($curl, CURLOPT_POST, true);
+			curl_setopt($curl, CURLOPT_URL, $req->get_normalized_http_url());
+			curl_setopt($curl, CURLOPT_POSTFIELDS, $req->to_postdata());
+		}
+		//curl_setopt($curl, CURLOPT_HTTPHEADER, array($req->to_header()));
+		$ret = curl_exec($curl);
+		if (!$ret) return false;
+		$code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+		if ($code!=200) return false;
+		return $ret;
+	}
+}
+
+class Jiwai {
+	private $auth;
+	function __construct(&$auth) {
+		$this->auth = &$auth;
+	}
+	function update($status) {
+		$ret = $this->auth->sendRequest('/statuses/update.json', 
+			array(
+				'status' => $status
+			), 'POST');
+		return $ret ? json_decode($ret) : false;
+	}
+	function publicTimeline($since = null) {
+		$ret = $this->auth->sendRequest('/statuses/public_timeline.json'.
+			($since ? '?since='.urlencode($since) : ''), 
+			array(
+			), 'GET');
+		return $ret ? json_decode($ret) : false;
+	}
+}
+?>
---
+++ nonweb/sdk/php/samples/index.php
@@ -0,0 +1,97 @@
+<h1>JiWai API 范例</h1>
+<?php
+require_once '../lib/Jiwai.php';
+$consumer_key    = '771020709341b1e110959e16012da225';
+$consumer_secret = '137b05030a3d9013e56072a547413369';
+session_start(); 
+function error($s) {
+?>
+<div style="background:lightgrey;color:red;text-align:center;"><?php echo $s; ?></div>
+<?php
+	die();
+}
+if (!empty($_POST['auth'])) $_SESSION['auth'] = $_POST['auth'];
+/*
+	auth	认证方式
+	'basic'	明文密码（不推荐）
+	'oauth'	OAuth
+*/
+if (empty($_SESSION['auth'])) {
+?>
+选择认证方式
+<form method="post">
+	<input type="radio" name="auth" value="basic" /> 明文密码（不推荐） <br />
+	<input type="radio" name="auth" value="oauth" /> OAuth <br />
+	<input type="submit" />
+</form>
+<?php
+	die();
+}
+
+if ($_SESSION['auth']=='basic') { //明文密码方式
+	if (!empty($_POST['username'])) $_SESSION['username'] = $_POST['username']; 
+	if (!empty($_POST['password'])) $_SESSION['password'] = $_POST['password']; 
+	if (empty($_SESSION['username'])||empty($_SESSION['password'])) {
+?>
+输入叽歪帐号信息
+<form method="post">
+	username <input type="text" name="username" value="" /> <br />
+	password <input type="password" name="password" value="" /> <br />
+	<input type="submit" />
+</form>
+<?php
+		die();
+	}
+	$auth = new Jiwai_Auth_Basic();
+	$auth->setLogin($_SESSION['username'], $_SESSION['password']);
+} else { //OAuth方式
+	$auth = new Jiwai_Auth_Oauth($consumer_key, $consumer_secret);
+	if (!empty($_GET['rtk']) && !empty($_SESSION['request_token']) && $_GET['rtk']==$_SESSION['request_token']->key) { //是否回调?
+		$auth->setToken($_SESSION['request_token']);
+		unset($_SESSION['request_token']);
+		$_SESSION['access_token'] = $auth->fetchAccessToken();
+		if (empty($_SESSION['access_token'])) error('访问令牌获取失败');
+	} elseif (!empty($_SESSION['access_token'])) { //已获得访问令牌?
+		$auth->setToken($_SESSION['access_token']);
+	} else { //获得一个请求令牌，并引导用户授权
+		$_SESSION['request_token'] = $auth->fetchRequestToken();
+		if (empty($_SESSION['request_token'])) error('请求令牌获取失败');
+		if (strpos($_SERVER['REQUEST_URI'], '?')!==false) $_SERVER['REQUEST_URI'] = substr($_SERVER['REQUEST_URI'], 0, strpos($_SERVER['REQUEST_URI'], '?'));
+		$url = $auth->getAuthorizeUrl('http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].'?rtk='.urlencode($_SESSION['request_token']->key));
+?>
+		<a href="<?php echo $url; ?>"> 点击开始认证</a>
+<?php
+		die();
+	}
+}
+
+$api = new Jiwai($auth);
+
+if (!empty($_POST['action'])) switch ($_POST['action']) {
+	case 'publictimeline':
+		var_dump($api->publicTimeline()); 
+		break;
+	case 'update': 
+		var_dump($api->update($_POST['status']));
+		break;
+	case 'logout':
+		$_SESSION=array();
+		header('Location: '.'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+		break;
+}
+?>
+<hr />
+<form method="post">
+	<input type="hidden" name="action" value="update" />
+	<input type="text" name="status" value="hehe" /> 
+	<input type="submit" value="叽歪一下" />
+</form>
+<form method="post">
+	<input type="hidden" name="action" value="publictimeline" />
+	<input type="submit" value="叽歪广场" />
+</form>
+<form method="post">
+	<input type="hidden" name="action" value="logout" />
+	<input type="submit" value="注销" />
+</form>
+
