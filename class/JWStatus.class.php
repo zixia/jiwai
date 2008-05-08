d5a5c04812c50465efa633ec512505d2b3d800c5
Qiang WANG <wqsemc@gmail.com>
2008-05-08 08:41:58 +0000

    See #467, By WqSemc, fix a bug of flickr url who contain @

--- class/JWStatus.class.php
+++ class/JWStatus.class.php
@@ -1147,7 +1147,7 @@ _HTML_;
 		}
 
 		// Add @ Link For other User
-		$status = preg_replace(	 '/@\s*([^\s<>,，:\$@#]{3,20})(,|，|:|\b|\s|$)/' ,"@<a href='/\\1/' rel='contact'>\\1</a>\\2" ,$status );
+		$status = preg_replace(	 '/\s@\s*([^\s<>,，:\$@#]{3,20})(,|，|:|\b|\s|$)/' ," @<a href='/\\1/' rel='contact'>\\1</a>\\2" ,$status );
 		$status = preg_replace(	 "/\[\s*([^<>@#\]\[]{3,20})\](|\b|\s|$)/" ,"[<a href='/t/\\1/' rel='tag'>\\1</a>]\\2" ,$status );
 
 		if( $conf_id ) 
