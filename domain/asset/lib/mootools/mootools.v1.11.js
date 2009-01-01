a5bffb4bfb0c9afb3843c190b33bef53183e007c
Shuwen DAI <shwdai@gmail.com>
2009-01-01 14:03:38 +0000

    see #501 js adjust

--- domain/asset/lib/mootools/mootools.v1.11.js
+++ domain/asset/lib/mootools/mootools.v1.11.js
@@ -996,8 +996,9 @@ var Garbage = {
 			if (el.$events) el.fireEvent('trash').removeEvents();
 			for (var p in el.$tmp) el.$tmp[p] = null;
 			for (var p in Element.prototype) el.p = null;
+			el.htmlElement = el.$tmp = null;
 			Garbage.elements[Garbage.elements.indexOf(el)] = null;
-			el = el.htmlElement = el.$tmp = null;
+			el = null;
 		}
 		Garbage.elements.remove(null);
 	},
