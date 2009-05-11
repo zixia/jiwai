0bc55a1b7b0246290f8837999d8f8616bc831592
Shuwen DAI <shwdai@gmail.com>
2009-05-11 03:06:43 +0000

    see #591 Add friend link www.artspy.cn

--- webroot/wo/about/partner.php
+++ webroot/wo/about/partner.php
@@ -88,6 +88,7 @@
 	<a href="http://www.pindao.com/" title="品道" target="_blank"><img width="88" height="31" src="<? echo JWTemplate::GetAssetUrl('/images/flink/pindao8831.gif');?>" alt="品道"></a>
 	<a href="http://www.cn939.com/" title="中国中医网" target="_blank"><img width="88" height="31" src="<? echo JWTemplate::GetAssetUrl('/images/flink/yiyao8831.gif');?>" alt="中国中医网"></a>
 	<a href="http://www.ipart.cn/" title="爱情公寓" target="_blank"><img width="88" height="31" src="<? echo JWTemplate::GetAssetUrl('/images/flink/ipart8831.gif');?>" alt="爱情公寓"></a>
+	<a href="http://www.artspy.cn/" title="艺术眼" target="_blank"><img width="88" height="31" src="<? echo JWTemplate::GetAssetUrl('/images/flink/artspy8831.jpg');?>" alt="艺术眼"></a>
 	<p style="float:left;width:100%;text-align:right;margin-top:13px;font-size:1.2em;color:#545454">友情链接排名不分先后</p>
 	 </div>
  </div>
